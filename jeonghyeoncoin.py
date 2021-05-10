75427e1bc0a11a256fc958210ad87a7705cdbbf5
Leohyeong <83483952+Leohyeong@users.noreply.github.com>
2021-05-10 10:44:16 +0900

    Update jeonghyeoncoin.py

--- jeonghyeoncoin.py
+++ jeonghyeoncoin.py
@@ -47,7 +47,7 @@ while True:
         if start_time < now < end_time - datetime.timedelta(seconds=10):
             target_price = get_target_price("KRW-DOGE", 0.2)
             current_price = get_current_price("KRW-DOGE")
-            if target_price > current_price:
+            if target_price < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
                     upbit.buy_market_order("KRW-DOGE", krw*0.9995)
