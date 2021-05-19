d86a0ac8f96191d0d88db48981821935c7717300
Leohyeong <83483952+Leohyeong@users.noreply.github.com>
2021-05-19 20:35:07 +0900

    Update buycoin.py

--- buycoin.py
+++ buycoin.py
@@ -91,12 +91,14 @@ def coin_autotrade(__krw_coin__,__coin_name__,__min_val__,__money__):
             if __money__ > 5000 and coin < __min_val__:
                 upbit.buy_market_order(__krw_coin__, __money__*fee)
                 __money__ = int(round((__money__ * fee),-1))
+                print("Buy :", __coin_name__ ," price :", str(__money__))
                 bot.sendMessage(chat_id=chat_id, text="Buy : "+__coin_name__+" price : "+str(__money__))
         else:
             coin = get_balance(__coin_name__)
             if coin > __min_val__:
                 upbit.sell_market_order(__krw_coin__, coin)
                 __money__ = coin * get_current_price(__krw_coin__)
+                print("Sell :", __coin_name__ ," price :", str(__money__))
                 bot.sendMessage(chat_id=chat_id, text="Sell : "+__coin_name__+" price : "+str(__money__))
         time.sleep(1)
     except Exception as e:
