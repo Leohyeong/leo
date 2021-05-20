1fd2a53429fe72a3e2efbdf9f886bbd5f6991cee
Leohyeong <83483952+Leohyeong@users.noreply.github.com>
2021-05-20 14:13:58 +0900

    Update coin.py

--- coin.py
+++ coin.py
@@ -1,59 +1,30 @@
 import time
 import pyupbit
 import telegram
+import math
 
 bot = telegram.Bot(token='1825271568:AAHLOTqYV8ZZ8IN0I1N-FpR68g0PGThQyVs')
 chat_id = 1893587930
 
-mybalance = 201433
 
 fee = 0.9995
 
-access = "OFCL17jSpSEAj3r1gnvHAGPMSix5MShrAcsz9Hi4"
-secret = "04fFGc0jmnpOupg3T2DfejiFGuojYiMFVwIPGiXU"
-
-
-doge_val = mybalance
-etc_val = mybalance
-eth_val = mybalance
-
-
 ## __KRW_coin__ ##
 
-krw_doge = "KRW-DOGE"
-krw_etc = "KRW-ETC"
 krw_eth = "KRW-ETH"
-krw_btc = "KRW-BTC"
-
 
 ## __coin_name__ ##
 
-doge = "DOGE"
-etc = "ETC"
-xrp = "XRP"
-btc = "BTC"
 eth = "ETH"
 
 ## __min_val__ ##
 
-doge_min = 7.7
-etc_min = 0.043
-xrp_min = 2.92
-btc_min = 0.000103
 eth_min = 0.0014
-
-
-## trade_time_set ##
-
-trade_minute1 = "minute1"
-trade_minute60 = "minute60"
-trade_minute40 = "minute240"
-
 
 ## function ##
 
 def get_ma20(ticker):
-    """20시간 이동 평균선 조회"""
+    """20분 이동 평균선 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="minute1", count=20)
     ma20 = df['close'].rolling(20).mean().iloc[-1]
     return ma20
@@ -79,19 +50,18 @@ def coin_autotrade(__krw_coin__,__coin_name__,__min_val__,__money__):
         ma20 = get_ma20(__krw_coin__)
         current_price = get_current_price(__krw_coin__)
         coin = get_balance(__coin_name__)
-        if ma20 <= current_price:
-            if __money__ > 5000 & coin < __min_val__:
+        if (ma20 <= current_price <= ma20+2000):
+            if (__money__ > 5000) & (coin < __min_val__):
                 upbit.buy_market_order(__krw_coin__, __money__*fee)
                 __money__ = float(round((__money__ * fee),-1))
-                print("Buy :", __coin_name__ ," price :", str(__money__))
+                print("Buy :", __coin_name__ ," price :", str(current_price))
                 bot.sendMessage(chat_id=chat_id, text="Buy : "+__coin_name__+" price : "+str(__money__))
-        else:
-            if coin > __min_val__:
+        elif (ma20 > current_price):
+            if (coin > __min_val__):
                 upbit.sell_market_order(__krw_coin__, coin)
                 __money__ = float(coin * get_current_price(__krw_coin__))
-                print("Sell :", __coin_name__ ," price :", str(__money__))
+                print("Sell :", __coin_name__ ," price :", str(current_price))
                 bot.sendMessage(chat_id=chat_id, text="Sell : "+__coin_name__+" price : "+str(__money__))
-        time.sleep(0.1)
     except Exception as e:
         print(e)
         time.sleep(1)
@@ -101,17 +71,20 @@ def coin_autotrade(__krw_coin__,__coin_name__,__min_val__,__money__):
 
 ## 로그인 ##
 
+access = "OFCL17jSpSEAj3r1gnvHAGPMSix5MShrAcsz9Hi4"
+secret = "04fFGc0jmnpOupg3T2DfejiFGuojYiMFVwIPGiXU"
+
 upbit = pyupbit.Upbit(access, secret)
 
-print("Auto Trade Start!!!")
+mybalance = float(math.floor(upbit.get_balance('KRW')))
+
+eth_val = mybalance
+
+print("ETH Trading...")
 
 
 # 자동매매 시작
 
 while True:
 
-    doge_val = coin_autotrade(krw_doge,doge,doge_min,doge_val)
     eth_val = coin_autotrade(krw_eth,eth,eth_min,eth_val)
-    etc_val = coin_autotrade(krw_etc,etc,etc_min,etc_val)
-
-#     xrp_val = coin_autotrade(krw_xrp,xrp,xrp_min,xrp_val)
