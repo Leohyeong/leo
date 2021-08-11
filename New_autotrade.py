745a17fda5971186aa980303d699061fff0ed378
Leohyeong <83483952+Leohyeong@users.noreply.github.com>
2021-08-11 18:02:11 +0900

    Update New_autotrade.py

--- New_autotrade.py
+++ New_autotrade.py
@@ -38,9 +38,32 @@ best_k = (k_tmp.index(max(k_tmp))+1)*0.1
 
 # print("\n" + str(best_k) + "\n")
 
+def backtest(name,days,k):
+    
+    df = pyupbit.get_ohlcv(name,count=days)
+    df['range'] = (df['high'] - df['low']) * k
+    df['target'] = df['open'] + df['range'].shift(1)
+
+    fee = 0.0005
+    df['ror_tempt'] = np.where(df['high'] > df['target'],
+                            df['close'] / df['target'] - fee,
+                            1)
+    df['hpr_tempt'] = df['ror_tempt'].cumprod() # 누적수익률
+
+    df['ror'] = 100 * (np.where(df['high'] > df['target'], # 수익률 // (조건문, 참일때 값, 거짓일때 값)
+                        df['close'] / df['target'] - fee,
+                        1) - 1)
+    df['hpr'] = 100 * (df['ror_tempt'].cumprod() - 1) # 누적수익률
+
+    df['dd'] = (df['hpr_tempt'].cummax() - df['hpr_tempt']) / df['hpr_tempt'].cummax() * 100 # (누적최대값과 현재 hpr / 누적 최대값 * 100)
+    print("MDD(%): ", df['dd'].max())
+    # df.to_excel("dd.xlsx")
+
+    print(df)
+
+backtest(name,days,best_k)
+
 ## Auto Trade ##
-
-
 
 # # KRW로 표기된 종목의 코드 확인
 tickers = pyupbit.get_tickers(fiat="KRW")
